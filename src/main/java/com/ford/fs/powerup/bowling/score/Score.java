8858edceeaebcb96bf39ce8c4f0ea907ba1c5364
ralluru <ralluru@ford.com>
2019-12-05 10:31:47 -0500

    Added logic to accept first frame and return the total.

--- src/main/java/com/ford/fs/powerup/bowling/score/Score.java
+++ src/main/java/com/ford/fs/powerup/bowling/score/Score.java
@@ -5,8 +5,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Score {
-
+    int totalScore = 0;
+    int prevRoll = 0;
+    int frameSize= 0;
     public int calculateScore(int pins) {
-        return pins;
+
+        if(frameSize == 0){
+            prevRoll = pins;
+            frameSize++;
+            return 0;
+        }
+        totalScore = prevRoll+pins;
+        return totalScore;
     }
 }
--- src/test/java/com/ford/fs/powerup/bowling/score/ScoreTest.java
+++ src/test/java/com/ford/fs/powerup/bowling/score/ScoreTest.java
@@ -2,25 +2,38 @@ package com.ford.fs.powerup.bowling.score;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 @SpringBootTest
 class ScoreTest {
 
-    @Autowired
     private Score score;
 
     @BeforeEach
     void setUp() {
+        score = new Score();
     }
 
     @Test
-    void calculateScore() {
+    void test_calculateScoreWhenZeroIsRolled() {
         int value = score.calculateScore(0);
-        assertEquals(0,value);
-        value = score.calculateScore(7);
-        assertEquals(7,value);
+        assertThat(value).isEqualTo(0);
     }
+
+    @Test
+    void test_calculateScoreWhenNonZeroAndNonSpecial() {
+        int value = score.calculateScore(7);
+        assertThat(value).isEqualTo(0);
+    }
+
+    @Test
+    void test_calculateScoreFor1Frame_withTwoRolls() {
+        score.calculateScore(1);
+        int value = score.calculateScore(2);
+        assertThat(value).isEqualTo(3);
+    }
+
 }
