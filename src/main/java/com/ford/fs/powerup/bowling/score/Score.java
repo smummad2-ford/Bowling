27eee9ce09f7f21244a1c0ad1cf65d56f512858a
ralluru <ralluru@ford.com>
2019-12-06 10:18:42 -0500

    Added logic to accept multpile frames

--- src/main/java/com/ford/fs/powerup/bowling/score/Score.java
+++ src/main/java/com/ford/fs/powerup/bowling/score/Score.java
@@ -7,15 +7,18 @@ import org.springframework.stereotype.Component;
 public class Score {
     int totalScore = 0;
     int prevRoll = 0;
-    int frameSize= 0;
+    int rollCount= 0;
     public int calculateScore(int pins) {
-
-        if(frameSize == 0){
-            prevRoll = pins;
-            frameSize++;
-            return 0;
+        if(pins>10){
+            throw new IllegalArgumentException("pin cannot be more than 10");
         }
-        totalScore = prevRoll+pins;
+        if(rollCount % 2 ==0){
+            prevRoll = pins;
+            rollCount++;
+            return totalScore;
+        }
+        totalScore += prevRoll + pins;
+        rollCount++;
         return totalScore;
     }
 }
--- src/test/java/com/ford/fs/powerup/bowling/score/ScoreTest.java
+++ src/test/java/com/ford/fs/powerup/bowling/score/ScoreTest.java
@@ -1,10 +1,12 @@
 package com.ford.fs.powerup.bowling.score;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
@@ -34,6 +36,31 @@ class ScoreTest {
         score.calculateScore(1);
         int value = score.calculateScore(2);
         assertThat(value).isEqualTo(3);
+    }
+
+    @Test
+    void test_too_many_rolls(){
+        IllegalArgumentException myException = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            score.calculateScore(11);
+        });
+        assertTrue(myException.getMessage().contains("pin cannot be more than 10"));
+    }
+
+    @Test
+    void test_calculateScoreFor1Frame_withThreeRolls() {
+        score.calculateScore(1);
+        score.calculateScore(2);
+        int value = score.calculateScore(5);
+        assertThat(value).isEqualTo(3);
+    }
+
+    @Test
+    void test_calculateScoreFor2Frames(){
+        score.calculateScore(1);
+        score.calculateScore(2);
+        score.calculateScore(5);
+        int value = score.calculateScore(3);
+        assertThat(value).isEqualTo(11);
     }
 
 }
