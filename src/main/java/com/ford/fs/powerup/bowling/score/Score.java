ecd4393aa53d358e9c45d45f29c9ba2e51bab14c
ralluru <ralluru@ford.com>
2019-12-11 10:31:02 -0500

    Initiated refactoring

--- src/main/java/com/ford/fs/powerup/bowling/score/Score.java
+++ src/main/java/com/ford/fs/powerup/bowling/score/Score.java
@@ -6,59 +6,84 @@ import org.springframework.stereotype.Component;
 @Component
 public class Score {
     int totalScore = 0;
-    int frameSize = 0;
     int rollCount = 0;
     int frameScore = 0;
-    boolean isSpare = false;
-    boolean isStrike = false;
+    boolean isPreviousRollSpare = false;
+    boolean isPreviousRollStrike = false;
 
 
     public int calculateScore(int pins) {
-        if(isStrike) {
-            if(rollCount%2 == 0) {
-                frameScore = pins;
-                rollCount++;
-                return totalScore;
-            }
-            frameScore += pins;
-            return totalScore + (frameScore * 2) + 10;
-        }
-
-        if (pins == 10) {
-            if (rollCount % 2 == 0) {
-                rollCount++;
-            }
-            rollCount++;
-            isStrike = true;
-            frameScore = pins;
-            return totalScore;
-        }
-
-        if (isSpare) {
-            totalScore += pins;
-            frameScore = pins;
-            isSpare = false;
-            rollCount++;
-            return totalScore;
-        }
         if (pins > 10) {
             throw new IllegalArgumentException("pin cannot be more than 10");
         }
-        if (rollCount % 2 == 0) {
+
+        if(isPreviousRollStrike) {
+            return handleAfterStrike(pins);
+        }
+
+        if (handleStrikeCase(pins)) return totalScore;
+
+        if(isPreviousRollSpare) {
+            return handleAfterSpareCase(pins);
+
+        }
+        if (isFirstRoll()) {
             frameScore = 0;
-            frameScore = pins;
-            rollCount++;
+            setFrameScoreAndIncrementRollCount(pins);
             return totalScore;
         }
 
         frameScore += pins;
         totalScore += frameScore;
         rollCount++;
-        if (frameScore == 10) {
-            isSpare = true;
-            return 0;
-        }
+
+        if (handleSpareCase()) return 0;
 
         return totalScore;
+    }
+
+    private boolean handleSpareCase() {
+        if (frameScore == 10) {
+            isPreviousRollSpare = true;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isFirstRoll() {
+        return rollCount % 2 == 0;
+    }
+
+    private int handleAfterStrike(int pins) {
+        if(isFirstRoll()) {
+            setFrameScoreAndIncrementRollCount(pins);
+            return totalScore;
+        }
+        frameScore += pins;
+        return totalScore + (frameScore * 2) + 10;
+    }
+
+    private boolean handleStrikeCase(int pins) {
+        if (pins == 10) {
+            if (isFirstRoll()) {
+                rollCount++;
+            }
+            isPreviousRollStrike = true;
+            setFrameScoreAndIncrementRollCount(pins);
+            return true;
+        }
+        return false;
+    }
+
+    private int handleAfterSpareCase(int pins) {
+            totalScore += pins;
+            isPreviousRollSpare = false;
+            setFrameScoreAndIncrementRollCount(pins);
+            return totalScore;
+    }
+
+    private void setFrameScoreAndIncrementRollCount(int pins) {
+        frameScore = pins;
+        rollCount++;
     }
 }
